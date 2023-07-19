aef77113ba3bd975ac757b8a8da17b3ca4ba2f17
luizduarte12 <luiz.duarte@sou.ufac.br>
2023-07-18 20:59:52 -0500

    Versão com oq ele quer nas questões 1, 2 e 3

---
+++ bfs_2.py
@@ -0,0 +1,59 @@
+from collections import deque
+
+def bfs(grafo, inicio, objetivos):
+    fila = deque([(inicio, [inicio])])
+    profundidade = {inicio: 0}
+    ramificacao = {inicio: 0}
+
+    while fila:
+        no_atual, caminho = fila.popleft()
+
+        if no_atual in objetivos:
+            custo = len(caminho) - 1
+            profundidade_objetivo = len(caminho) - 1
+            ramificacao_objetivo = len(grafo[no_atual])
+            print(f"Caminho para o objetivo (nó '{no_atual}'): {caminho}")
+            print(f"Custo da solução: {custo}")
+            print(f"Profundidade do objetivo: {profundidade_objetivo}")
+            print(f"Ramificação máxima: {ramificacao_objetivo}")
+            print(f"Ramificação média: {sum(ramificacao.values()) / len(ramificacao):.2f}")
+            return caminho
+
+        for vizinho in grafo[no_atual]:
+            if vizinho not in caminho:
+                fila.append((vizinho, caminho + [vizinho]))
+                profundidade[vizinho] = profundidade[no_atual] + 1
+                ramificacao[vizinho] = len(grafo[vizinho])
+
+    return None
+
+# Exemplo de utilização
+if __name__ == "__main__":
+    grafo = {
+        '6': ['1', '9', '7'],
+        '1': ['2'],
+        '9': ['10'],
+        '7': ['10', '2'],
+        '2': ['3'],
+        '10': [],
+        '3': ['4', '8'],
+        '4': ['5'],
+        '5': [],
+        '8': ['11'],
+        '11': ['12', '13'],
+        '12': [],
+        '13': ['17'],
+        '17': ['18', '16'],
+        '16': ['15'],
+        '15': ['14'],
+        '14': [],
+        '18': []
+    }
+
+    inicio = '6'
+    objetivos = ['18', '14']
+
+    caminho_objetivo = bfs(grafo, inicio, objetivos)
+
+    if not caminho_objetivo:
+        print("Não há caminho para nenhum dos objetivos a partir do nó inicial.")
